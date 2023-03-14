1ad427edbb6b4958b83dd20c3bd7007df23910bd
Ming-z-y <2740042735@qq.com>
2023-03-14 10:49:43 +0800

    feat: add the search loading

--- lib/searchPage/search.dart
+++ lib/searchPage/search.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
 
+import '../utils/refreshComponent.dart';
+
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
@@ -64,165 +66,169 @@ class _SearchPage extends State<SearchPage> {
         body: Container(
           padding: const EdgeInsets.all(10),
           child: EasyRefresh(
+              onRefresh: () async {},
+              onLoad: () async {},
+              header: CustomRefreshHeader(refreshText: '下拉重新加载'),
+              footer: CustomRefreshFooter(),
               child: ListView(
-            children: [
-              // 历史记录字体
-              Container(
-                padding: const EdgeInsets.only(left: 5),
-                margin: const EdgeInsets.all(10),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    left: BorderSide(
-                        width: 5, color: Color.fromRGBO(130, 128, 239, 1)),
-                  ),
-                ),
-                child: const Text(
-                  '历史记录',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Color.fromRGBO(51, 51, 51, 1),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 10, right: 10),
-                child: Wrap(
-                  children: [
-                    // 样例
-                    Container(
-                      margin: const EdgeInsets.only(right: 20),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color.fromRGBO(246, 246, 246, 1)),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                        onPressed: (() {}),
-                        child: Text(
-                          '男生如何护肤',
-                          style:
-                              TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
-                        ),
+                children: [
+                  // 历史记录字体
+                  Container(
+                    padding: const EdgeInsets.only(left: 5),
+                    margin: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        left: BorderSide(
+                            width: 5, color: Color.fromRGBO(130, 128, 239, 1)),
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(right: 20),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color.fromRGBO(246, 246, 246, 1)),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                        onPressed: (() {}),
-                        child: Text(
-                          '男生医美靠谱吗，会不会...',
-                          style:
-                              TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
-                        ),
+                    child: const Text(
+                      '历史记录',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(51, 51, 51, 1),
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(right: 20),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color.fromRGBO(246, 246, 246, 1)),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    child: Wrap(
+                      children: [
+                        // 样例
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color.fromRGBO(246, 246, 246, 1)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            onPressed: (() {}),
+                            child: Text(
+                              '男生如何护肤',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(90, 76, 112, 1)),
                             ),
                           ),
                         ),
-                        onPressed: (() {}),
-                        child: Text(
-                          '脸部祛痘用什么好',
-                          style:
-                              TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color.fromRGBO(246, 246, 246, 1)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            onPressed: (() {}),
+                            child: Text(
+                              '男生医美靠谱吗，会不会...',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(90, 76, 112, 1)),
+                            ),
+                          ),
                         ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color.fromRGBO(246, 246, 246, 1)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            onPressed: (() {}),
+                            child: Text(
+                              '脸部祛痘用什么好',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(90, 76, 112, 1)),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  // 猜你想搜字体
+                  Container(
+                    padding: const EdgeInsets.only(left: 5),
+                    margin: const EdgeInsets.only(top: 30, left: 10),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        left: BorderSide(
+                            width: 5, color: Color.fromRGBO(130, 128, 239, 1)),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              // 猜你想搜字体
-              Container(
-                padding: const EdgeInsets.only(left: 5),
-                margin: const EdgeInsets.only(top: 30, left: 10),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    left: BorderSide(
-                        width: 5, color: Color.fromRGBO(130, 128, 239, 1)),
+                    ),
+                    child: const Text(
+                      '猜你想搜',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(51, 51, 51, 1),
+                      ),
+                    ),
                   ),
-                ),
-                child: const Text(
-                  '猜你想搜',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Color.fromRGBO(51, 51, 51, 1),
+                  // 样例
+                  SizedBox(
+                    height: 40,
+                    child: ListTile(
+                      title: const Text(
+                        '男神逆袭计划',
+                        style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                      ),
+                      onTap: () {
+                        print("点击");
+                      },
+                    ),
                   ),
-                ),
-              ),
-              // 样例
-              SizedBox(
-                height: 40,
-                child: ListTile(
-                  title: const Text(
-                    '男神逆袭计划',
-                    style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                  SizedBox(
+                    height: 40,
+                    child: ListTile(
+                      title: const Text(
+                        '男性不动刀可以做的医美',
+                        style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                      ),
+                      onTap: () {
+                        print("点击");
+                      },
+                    ),
                   ),
-                  onTap: () {
-                    print("点击");
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 40,
-                child: ListTile(
-                  title: const Text(
-                    '男性不动刀可以做的医美',
-                    style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                  SizedBox(
+                    height: 40,
+                    child: ListTile(
+                      title: const Text(
+                        '提高气质小技巧',
+                        style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                      ),
+                      onTap: () {
+                        print("点击");
+                      },
+                    ),
                   ),
-                  onTap: () {
-                    print("点击");
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 40,
-                child: ListTile(
-                  title: const Text(
-                    '提高气质小技巧',
-                    style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                  SizedBox(
+                    height: 40,
+                    child: ListTile(
+                      title: const Text(
+                        '寒风穿搭，博主手把手教学',
+                        style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
+                      ),
+                      onTap: () {
+                        print("点击");
+                      },
+                    ),
                   ),
-                  onTap: () {
-                    print("点击");
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 40,
-                child: ListTile(
-                  title: const Text(
-                    '寒风穿搭，博主手把手教学',
-                    style: TextStyle(color: Color.fromRGBO(90, 76, 112, 1)),
-                  ),
-                  onTap: () {
-                    print("点击");
-                  },
-                ),
-              ),
-            ],
-          )),
+                ],
+              )),
         ));
   }
 }
